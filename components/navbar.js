4d48da4d34b9d72fd97e0701da42aad42c121a3b
sinyorre <sinanalagoz999@gmail.com>
2020-05-10 21:58:21 +0300

    add category

--- components/navbar.js
+++ components/navbar.js
@@ -102,12 +102,31 @@ function mNavbar() {
                                 <NavDropdown.Item onClick={() => closeAuth()}>Çıkış Yap</NavDropdown.Item>
                             </NavDropdown>
                         ] : [
-                            <Button variant="danger" style={{ marginLeft: '3px' }} onClick={() => auth()}>
-                                <FaGoogle /> Giriş Yap
+                                <Button variant="danger" style={{ marginLeft: '3px' }} onClick={() => auth()}>
+                                    <FaGoogle /> Giriş Yap
                             </Button>
                             ]
                     }
-
+                    <NavDropdown title="Kategoriler" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/filmler/kategori/aksiyon-filmleri/1">Aksiyon</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/macera-filmleri/1">Macera</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/animasyon-filmleri/1">Animasyon</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/komedi-filmleri/1">Komedi</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/suc-filmleri/1">Suç</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/belgesel-filmleri/1">Belgesel</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/dram-filmleri/1">Dram</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/aile-filmleri/1">Aile</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/fantastik-filmleri/1">Fantastik</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/tarih-filmleri/1">Tarih</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/korku-filmleri/1">Korku</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/müzik-filmleri/1">Müzik</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/gizem-filmleri/1">Gizem</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/romantik-filmler/1">Romantik</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/bilim-kurgu-filmleri/1">Bilim Kurgu</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/gerilim-filmleri/1">Gerilim</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/tarih-filmleri/1">Tarih</NavDropdown.Item>
+                        <NavDropdown.Item href="/filmler/kategori/vahsi-bati-filmleri/1">Vahşi Batı</NavDropdown.Item>
+                    </NavDropdown>
 
 
 
@@ -115,7 +134,7 @@ function mNavbar() {
                 <Form inline>
                     <FormControl type="text" placeholder="Örn: Yüzüklerin Efendisi" className="mr-sm-2"
                         value={movieSearch} onChange={(event) => setMovieSearhc(event.target.value)} />
-                    <Button variant="danger" onClick={() => search()} style={{marginTop:'2px;'}}>Ara</Button>
+                    <Button variant="danger" onClick={() => search()} style={{ marginTop: '2px;' }}>Ara</Button>
                 </Form>
             </Navbar.Collapse>
         </Navbar>
---
+++ pages/filmler/kategori/[categoryName]/[page].js
@@ -0,0 +1,188 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Pagination from 'react-bootstrap/Pagination'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Badge from 'react-bootstrap/Badge'
+import Navbar from '../../../../components/navbar'
+import Footer from '../../../../components/footer'
+
+const Category = (props) => {
+  const router = useRouter()
+  const categoryName = router.query.categoryName
+  let page = router.query.page
+
+  const movieData = props.data;
+  const total_pages = movieData.total_pages;
+
+  page = parseInt(page);
+  let active = page;
+  let items = [];
+  for (let number = page - 4; number <= page + 4; number++) {
+    if (number > 0) {
+      items.push(
+        <Pagination.Item key={number} active={number === active}>
+          <Link href={`/filmler/kategori/${categoryName}/${number}`}>
+            <a>{number}</a>
+          </Link>
+        </Pagination.Item>,
+      );
+    }
+  }
+
+  return (
+    <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
+      <Navbar />
+
+      <Container>
+        <Row>
+            <h1>{categoryName.replace("-"," ")} filmleri</h1>
+        </Row>
+        <Row style={{display:'flex', justifyContent: 'center'}}>
+          {movieData.results.map(item => (
+            <Card style={{
+              width: '150px',
+              height: 'auto', whiteSpace: 'pre-wrap',
+              display: 'inline-block', verticalAlign: 'top', border: 'none',
+              marginRight: '10px', overflow: 'hidden',
+              backgroundColor: '#1a1a1a',
+              marginTop: '10px'
+            }}>
+              {
+                <Card.Link href={item.href}>
+                  <Card.Img rounded variant="top" src={`http://image.tmdb.org/t/p/w185${item.poster_path}`} style={{ objectFit: 'fill' }} />
+                </Card.Link>
+              }
+
+
+              <Card.Body>
+                {
+
+                  <Card.Link href={`/filmler/${item.original_title.split(' ').join('-')}-${item.id}`}>
+                    <Card.Subtitle className="mb-2 text-muted">{item.title}</Card.Subtitle>
+                  </Card.Link>
+                }
+              </Card.Body>
+
+
+            </Card>
+          ))}
+        </Row>
+        <Row style={{ marginTop: '15px', marginBottom: '10px' }}>
+          <Pagination style={{margin: 'auto' }} className="pagination">
+            <Pagination.Item>
+              <Link href={`/filmler/kategori/${categoryName}/1`}>
+                <a>1</a>
+              </Link>
+            </Pagination.Item>
+            <Pagination.Ellipsis />
+            {items}
+            <Pagination.Ellipsis />
+            <Pagination.Item>
+              <Link href={`/filmler/kategori/${categoryName}/${total_pages}`}>
+                <a>{total_pages}</a>
+              </Link>
+            </Pagination.Item>
+          </Pagination>
+        </Row>
+
+        <Footer />
+      </Container>
+
+      <style global jsx>{`
+                body {
+                  margin:0px; 
+                  padding:0px;
+                  background-color: #000000;
+                }
+                .pagination{
+
+                }
+                a{
+                  color:#ffffff;
+                }
+                a:hover{
+                  color:#dc3545;
+                }
+                .page-link, .page-item{
+                  background-color: #1a1a1a;
+                }
+                .pagination > li > a
+                {
+                    background-color: white;
+                    border:none;
+                    color:#ffffff;
+                    background-color: #1a1a1a
+                }
+                h1{
+                  color:#ffffff;
+                }
+            `}
+      </style>
+    </Container>
+  )
+}
+
+export async function getServerSideProps({ query }) {
+  const page = query.page;
+  const categoryName = query.categoryName.split("-")[0];
+  let categoryId;
+  if (categoryName == "aksiyon") {
+    categoryId = "28";
+  } else if (categoryName == "macera") {
+    categoryId = "12";
+  } else if (categoryName == "animasyon") {
+    categoryId = "16";
+  } else if (categoryName == "komedi") {
+    categoryId = "35";
+  } else if (categoryName == "suc") {
+    categoryId = "80";
+  } else if (categoryName == "belgesel") {
+    categoryId = "99";
+  } else if (categoryName == "dram") {
+    categoryId = "18";
+  } else if (categoryName == "aile") {
+    categoryId = "10751";
+  } else if (categoryName == "fantastik") {
+    categoryId = "14";
+  } else if (categoryName == "tarih") {
+    categoryId = "36";
+  } else if (categoryName == "korku") {
+    categoryId = "27";
+  } else if (categoryName == "müzik") {
+    categoryId = "10402";
+  } else if (categoryName == "gizem") {
+    categoryId = "9648";
+  } else if (categoryName == "romantik") {
+    categoryId = "10749";
+  } else if (categoryName == "bilim") {
+    categoryId = "878";
+  } else if (categoryName == "gerilim") {
+    categoryId = "53";
+  } else if (categoryName == "savas") {
+    categoryId = "10752";
+  } else if (categoryName == "vahsi") {
+    categoryId = "37";
+  } 
+ 
+  const dateObj = new Date();
+  let month = dateObj.getUTCMonth() + 1;
+  let day = dateObj.getUTCDate();
+  const year = dateObj.getUTCFullYear();
+  if (month < 10) month = `0${month}`;
+
+  const today = `${year}-${month}-${day}`;
+
+  const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=94ec2f0211fe06a3b2bc9827439383d8&
+  language=tr-TR&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=${page}&release_date.lte=${today}&with_genres=${categoryId}`)
+  const data = await res.json()
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
+
+export default Category
